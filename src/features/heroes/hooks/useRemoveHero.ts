e01f8bff5a77177e078608545fefa861d45ecfa8
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-12 13:58:20 +0200

    update

--- src/features/heroes/hooks/useRemoveHero.ts
+++ src/features/heroes/hooks/useRemoveHero.ts
@@ -12,6 +12,10 @@ export default function useRemoveHero() {
       return deleteAxios<void>(EndPoints.heroes, heroId);
     },
     {
+      // If the mutation fails, use the context returned from onMutate to roll back
+      onError: (err, variables, context) => {
+        if (context?.backup) queryClient.setQueryData<HeroModel[]>([keys.heroes], context.backup.data);
+      },
       onMutate: async (heroId: string) => {
         // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
         await queryClient.cancelQueries([keys.heroes]);
