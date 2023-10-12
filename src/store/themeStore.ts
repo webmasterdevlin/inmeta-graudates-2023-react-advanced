77c71db5bef2b1e6bf4f778e3a9a364327a788dc
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-12 12:16:36 +0200

    update

--- src/store/themeStore.ts
+++ src/store/themeStore.ts
@@ -15,38 +15,46 @@ export type ThemeStoreType = {
 };
 
 export const useThemeStore = create<ThemeStoreType, any>(
-  persist(
-    (set, get) => {
-      const theme = {
-        isDark: false,
-        user: 'John Doe',
-      }; // useState ----> useStore
+  devtools(
+    persist(
+      (set, get) => {
+        const theme = {
+          isDark: false,
+          user: 'John Doe',
+        }; // useState ----> useStore
 
-      // without immer
-      const setLightTheme = () => {
-        return set(_ => {
-          return {
-            theme: { ...get().theme, isDark: false },
-          };
-        });
-      };
-      // with immer library
-      const setDarkTheme = () => {
-        return set(
-          produce((draft: Draft<ThemeStoreType>) => {
-            draft.theme.isDark = true;
-          }),
-        );
-      };
+        // without immer
+        const setLightTheme = () => {
+          return set(
+            _ => {
+              return {
+                theme: { ...get().theme, isDark: false },
+              };
+            },
+            false, // This is just a configuration of the behaviour of zustand with devtools
+            'setLightTheme',
+          );
+        };
+        // with immer library
+        const setDarkTheme = () => {
+          return set(
+            produce((draft: Draft<ThemeStoreType>) => {
+              draft.theme.isDark = true;
+            }),
+            false,
+            'setDarkTheme',
+          );
+        };
 
-      return {
-        setDarkTheme,
-        setLightTheme,
-        theme,
-      };
-    },
-    {
-      name: 'themeStore',
-    },
+        return {
+          setDarkTheme,
+          setLightTheme,
+          theme,
+        };
+      },
+      {
+        name: 'themeStore',
+      },
+    ),
   ),
 );
