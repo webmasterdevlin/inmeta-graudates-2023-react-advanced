c80ba6bb020f90bcd777458b281f9179a205a72c
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-10-13 12:10:22 +0200

    update

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,8 +1,27 @@
-import { QueryClient } from '@tanstack/react-query';
-import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render as rtlRender } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
+import type { QueryProviderWrapperProps } from './create-query-provider-wrapper';
+import type { ReactElement } from 'react';
 
 const queryClient = new QueryClient();
 
-const render = (ui: ReactElement, {...renderOptions} = {}) => {
-    const wrapper = ({children}: QueryProviderWrapperProps)
-}
+const render = (ui: ReactElement, { ...renderOptions } = {}) => {
+  const wrapper = ({ children }: QueryProviderWrapperProps) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <>
+            <NavigationBar />
+            <>{children}</>
+          </>
+        </BrowserRouter>
+      </QueryClientProvider>
+    );
+  };
+  return rtlRender(ui, { wrapper, ...renderOptions });
+};
+export * from '@testing-library/react';
+// override render method
+export { render };
